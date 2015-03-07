797d3d4b32ab6628b6b4a7650602a564962c9cd8
Fabio-CS <fabio.cs3mx@gmail.com>
2015-03-06 18:59:36 -0600

    Modifications

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
         <!-- ===================== css ===================== -->
         <link rel="stylesheet" href="css/layout.css" type="text/css">
         <!-- ===================== scripts ===================== -->
-        <script src="js/jquery.js" type="text/javascript"></script>
+        <script src="js/jquery-1.11.2.min.js" type="text/javascript"></script>
         <script src="js/scripts.js" type="text/javascript"></script>
         <!-- ===================== favicon ===================== -->
         <link href=img/favicon.ico rel="shortcut icon" />
@@ -28,13 +28,49 @@
         </div>
         <div id="div_menu">
             <ul>
-                <li><a href="#header">Home</a></li>
+                <li class="first"><a href="#header">Home</a></li>
                 <li><a href="#portfolio">Portf&oacute;lio</a></li>
                 <li><a href="#servicos">Servi&ccedil;os</a></li>
                 <li><a href="#combos">Combos</a></li>
                 <li><a href="#quemSomos">Quem Somos</a></li>
                 <li><a href="#contato">Contato</a></li>
             </ul>
+        </div>
+        <div id="div_home">
+            <h5>A Gongo</h5>
+            <p>
+                A Gongo Design e Comunicação é uma agência que alia planejamento, marketing estratégico e ações criativas de desenvolvimento para assim formar grandes marcas,
+                de forma estruturada e objetiva. Nossos projetos são desenvolvidos por uma equipe multidisciplinar que trabalha para resolver a necessidade de design e comunicação
+                visual dos clientes de forma criativa e inteligente, proporcionando experiências envolventes e criando conexões para comunicar e engajar o público-alvo.
+            </p>
+        </div>
+        <div id="div_slider">
+            
+        </div>
+        <div id="div_servicos">
+            <h3 id="servicos"><img src="img/square_yellow.png" alt="Serviços"/> Serviços</h3>
+            <div id="div_icons_servicos">
+                <div id="div_icon_design_grafico">
+                    <img src="img/icon_design_grafico.png" alt="Design Gráfico" /><br>
+                    <p>Design Gráfico</p>
+                    <img src="img/btn_mais.png" alt="mais" />
+                </div>
+                <div id="div_icon_web_design">
+                    <img src="img/icon_web_design.png" alt="Web Design" /><br>
+                    <p>Web Design</p>
+                    <img src="img/btn_mais.png" alt="mais" />
+                </div>
+                <div id="div_icon_marketing">
+                    <img src="img/icon_marketing_digital.png" alt="Marketing" /><br>
+                    <p>Marketing</p>
+                    <img src="img/btn_mais.png" alt="mais" />
+                </div>
+                <div id="div_icon_midias_sociais" class="last">
+                    <img src="img/icon_midias_sociais.png" alt="Mídias Sociais" /><br>
+                    <p>Mídias Sociais</p>
+                    <img src="img/btn_mais.png" alt="mais" />
+                </div>
+            </div>
         </div>
     </body>
 </html>
